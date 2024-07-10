ece33e69dcd63346f7bf67cf162fa1d1dc836209
arodriguez <angelarodriguezportela@gmail.com>
2024-07-10 12:10:43 +0200

    feat: add user entity

---
+++ src/main/java/com/anrdez/userservice/model/entity/User.java
@@ -0,0 +1,35 @@
+package com.anrdez.userservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+}
